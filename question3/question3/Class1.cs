8341bfef243c39b6f715bbb4859308e00fc36554
mohamedissam6 <112808440+mohamedissam6@users.noreply.github.com>
2022-09-04 20:42:05 +0200

    Add files via upload

---
+++ question3/question3/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question3
+{
+    internal class Shape
+    {
+        private float length;
+        private float width;
+
+        public float Length
+        {
+            set
+            {
+                if (value>0)length = value;
+            }
+            get { return length; }
+        }
+
+        public float Width
+        {
+            set
+            {
+                if (value > 0) width = value;
+            }
+            get { return width; }
+        }
+        public Shape(float length, float width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public string Check()
+        {
+            if (width == length) return "square";
+            else return "rectangle";
+        }
+
+        public static Shape operator +(Shape x, Shape y)
+        {
+            return new Shape((x.Length)+(y.length),(x.Width)+(y.Width));
+        }
+    }
+}
---
+++ question3/question3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shap1 = new Shape(5, 10);
+            Shape shap2 = new Shape(10, 5);
+            Shape shap3 = shap1 + shap2;
+            Console.WriteLine("{0}\t{1}\n{2}\t{3}\n{4}\t{5}",shap1.Length, shap1.Width,shap2.Length,shap2.Width,shap3.Length,shap3.Width);
+            Console.WriteLine(shap3.Check());
+            Console.ReadLine();
+             
+            
+
+        }
+    }
+}
